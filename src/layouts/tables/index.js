1cff6df5dc677663fb736b9a8f8e150e3ae971e3
JuinEsp <franciscoanto@gmail.com>
2022-11-29 16:30:00 -0300

    #5 - Add botão de detalhes e formatado espaços

--- src/layouts/tables/index.js
+++ src/layouts/tables/index.js
@@ -1,14 +1,14 @@
 // @mui material components
-// import Grid from "@mui/material/Grid";
-// import Card from "@mui/material/Card";
+import Grid from "@mui/material/Grid";
+import Card from "@mui/material/Card";
 
 // Material Dashboard 2 React components
 import Button from '@mui/material/Button';
-import Card from "@mui/material/Card"
-// import MDTypography from "components/MDTypography";
+import MDBox from "components/MDBox"
+import MDTypography from "components/MDTypography"
 import Box from '@mui/material/Box';
 import { DataGrid, GridToolbarContainer} from '@mui/x-data-grid';
-import EditIcon from '@mui/icons-material/Edit';
+import AddCircleIcon from '@mui/icons-material/AddCircle';
 
 // Material Dashboard 2 React example components
 import DashboardLayout from "examples/LayoutContainers/DashboardLayout";
@@ -28,16 +28,22 @@ function Tables() {
   
     return (
       <GridToolbarContainer>
-        <Button onClick={handleGoToPage1}>Go to page 1</Button>
+        <Button onClick={handleGoToPage1}><AddCircleIcon /></Button>
       </GridToolbarContainer>
     );
   };
 
   const columns = [
     {
+      field: 'realization',
+      headerName: 'Realizado',
+      width: 120,
+      editable: false,
+    },
+    {
       field: 'firstName',
       headerName: 'Membro',
-      width: 350,
+      width: 150,
       editable: false,
     },
     {
@@ -49,7 +55,7 @@ function Tables() {
     {
       field: 'gc',
       headerName: 'Nome Grupo',
-      width: 200,
+      width: 150,
       editable: false,
     },
     {
@@ -57,40 +63,65 @@ function Tables() {
       headerName: 'Detalhes',
       description: 'Click para saber mais informações desse grupo',
       sortable: false,
-      width: 160,
+      width: 100,
       renderCell: CustomToolbar,
     },
   ];
 
   const rows = [
-    { id: 1, lastName: 'Líder', firstName: 'Jon', gc: 'Gios' },
-    { id: 2, lastName: 'Pastor', firstName: 'Cersei', gc: 'Solao' },
-    { id: 3, lastName: 'Presbítero', firstName: 'Jaime', gc: 'Keyce' },
-    { id: 4, lastName: 'Líder', firstName: 'Arya', gc: 'Mayhe' },
-    { id: 5, lastName: 'Presbítero', firstName: 'Daenerys', gc: 'Luoln' },
-    { id: 6, lastName: 'Líder', firstName: null, gc: 'Ispan' },
-    { id: 7, lastName: 'Líder', firstName: 'Ferrara', gc: 'Bokdo' },
-    { id: 8, lastName: 'Líder', firstName: 'Rossini', gc: 'Faometu' },
-    { id: 9, lastName: 'Pastor', firstName: 'Harvey', gc: 'Yamkiog' },
+    { id: 1, lastName: 'Líder', firstName: 'Jon', gc: 'Gios' , realization: '19/11/2022'},
+    { id: 2, lastName: 'Pastor', firstName: 'Cersei', gc: 'Solao' , realization: '05/11/2022' },
+    { id: 3, lastName: 'Presbítero', firstName: 'Jaime', gc: 'Keyce'  , realization: '05/11/2022'},
+    { id: 4, lastName: 'Líder', firstName: 'Arya', gc: 'Mayhe'  , realization: '19/11/2022'},
+    { id: 5, lastName: 'Presbítero', firstName: 'Daenerys', gc: 'Luoln'  , realization: '04/11/2022'},
+    { id: 6, lastName: 'Líder', firstName: null, gc: 'Ispan' , realization: '05/11/2022' },
+    { id: 7, lastName: 'Líder', firstName: 'Ferrara', gc: 'Bokdo' , realization: '19/11/2022'},
+    { id: 8, lastName: 'Líder', firstName: 'Rossini', gc: 'Faometu' , realization: '19/11/2022' },
+    { id: 9, lastName: 'Pastor', firstName: 'Harvey', gc: 'Yamkiog' , realization: '04/11/2022' },
   ];
 
   return (
     <DashboardLayout>
       <DashboardNavbar />
-        <Card>
-        <Box sx={{ height: 400, width: '100%' }}>
-          <DataGrid
-            rows={rows}
-            columns={columns}
-            pageSize={5}
-            rowsPerPageOptions={[5]}
-            // checkboxSelection
-            // disableSelectionOnClick
-            experimentalFeatures={{ newEditingApi: true }}
-            
-          />
-        </Box>
+      <MDBox pt={6} pb={3}>
+      <Grid container spacing={6}>
+      <Grid item xs={12}>
+      <Card>
+      <MDBox
+                mx={2}
+                mt={-3}
+                py={3}
+                px={2}
+                variant="gradient"
+                bgColor="dark"
+                borderRadius="lg"
+                coloredShadow="info"
+              >
+                <MDTypography variant="h6" color="white">
+                  Lista de Grupo de Convívio
+                </MDTypography>
+              </MDBox>
+              <MDBox pt={3}>
+              <Card>
+          <Box sx={{ height: 400, width: '100%' }}>
+            <DataGrid
+              rows={rows}
+              columns={columns}
+              pageSize={5}
+              rowsPerPageOptions={[5]}
+              // checkboxSelection
+              // disableSelectionOnClick
+              experimentalFeatures={{ newEditingApi: true }}
+              
+            />
+          </Box>
         </Card>
+                </MDBox>
+        </Card>
+        </Grid>
+
+        </Grid>
+        </MDBox>
       <Footer />
     </DashboardLayout>
   );
