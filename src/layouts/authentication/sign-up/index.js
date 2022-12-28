b82ec5e78d64e725d6f42b2a5cc425b40aeb1fc7
JuinEsp <franciscoanto@gmail.com>
2022-12-28 12:08:47 -0300

    #10 - validados campos e enviando via request

--- src/layouts/authentication/sign-up/index.js
+++ src/layouts/authentication/sign-up/index.js
@@ -1,4 +1,6 @@
 import React, { useState, useEffect } from "react"
+import InputMask from "react-input-mask"
+import { useSnackbar } from "notistack"
 import { api } from "./../../../services/Api"
 // @mui material components
 import Grid from "@mui/material/Grid"
@@ -9,6 +11,8 @@ import InputLabel from '@mui/material/InputLabel'
 import Select from '@mui/material/Select'
 import MenuItem from "@mui/material/MenuItem"
 import FormControl from '@mui/material/FormControl';
+import TextField from '@mui/material/TextField';
+
 // Material Dashboard 2 React components
 import MDBox from "components/MDBox"
 import MDTypography from "components/MDTypography"
@@ -20,10 +24,12 @@ import '../../style/all.css'
 
 function Cover() {
   //PARA O USO DO SELECT
-  const [age, setAge] = useState('age');
-  const [group, setGroup] = useState('')
+  const [nameUser, setNameUser] = useState('')
+  const [idGroup, setIdGroup] = useState('')
   const [nameGroup, setNameGroup] = useState([])
-  const [showName, setShowName] = useState(false)
+  const [showName, setShowName] = useState(true)
+  const [birthDay, setBirthDay] = useState('')
+  const { enqueueSnackbar } = useSnackbar()
 
   const getGroups = () => {
     api.get('group')
@@ -41,7 +47,8 @@ function Cover() {
 
   const handleChange = (event) => {
     console.log(event.target.value);
-    setAge(event.target.value);
+    setIdGroup(event.target.value)
+
     if (event.target.value > 0) {
       console.log('nome completo')
       setShowName(true)  
@@ -51,6 +58,39 @@ function Cover() {
       setShowName(false)  
     }
   };
+
+  const onChangeDate = (event) => {
+    console.log('onChangeDate' , event.target.value);
+    setBirthDay(event.target.value)
+  }
+
+  const salvarDados = () => {
+    if(idGroup == "" || nameUser == "" || birthDay == "" )
+    {
+      enqueueSnackbar('Ops!!! Todos os campos são obrigatórios.',{ 
+        autoHideDuration: 3500,
+        variant: 'error',
+        anchorOrigin: {
+          horizontal: 'center',
+          vertical: 'bottom'
+        }
+      });
+    }
+
+    var dataPost = {
+      group_id : idGroup,
+      name: nameUser,
+      birthDate: birthDay
+    };
+    console.log({dataPost})
+    api.post('create-user', dataPost)
+    .then((res)=> {
+      console.log({res})
+    })
+    .catch((err) => {
+        console.log({err})
+    })
+  }
 
   return (
     <PageLayout>
@@ -73,77 +113,109 @@ function Cover() {
                   >Queremos conhecer um pouco mais sobre você.
                   </MDTypography>
                 </MDBox>
-                <Card>
-                  <MDBox
-                    variant="gradient"
-                    bgColor="dark"
-                    borderRadius="lg"
-                    coloredShadow="info"
-                    mx={2}
-                    mt={-3}
-                    p={2}
-                    mb={1}
-                    textAlign="center"
-                  >
-                    <MDTypography variant="h5" fontWeight="medium" color="white" mt={1}>
-                      Diga-nos qual o grupo você está!
-                    </MDTypography>
-                  </MDBox>
-                  <MDBox px={3} >
-                    <FormControl variant="standard" fullWidth sx={{ m: 1, minWidth: 120 }}>
-                      <InputLabel sx={{ p: 1 }}>Selecione um grupo</InputLabel>
-                      <Select
-                        labelId="demo-simple-select-standard-label"
-                        id="demo-simple-select-standard"
-                        value={age}
-                        onChange={handleChange}
-                        label="Selecione um grupo"
-                        fullWidth
-                        sx={{
-                          minHeight: '40px',
-                          mt: 1
-                        }}
-                      >
-                        <MenuItem value="0">
-                          <em>--Selecione--</em>
-                        </MenuItem>
-                      {
-                        nameGroup?.map(element => {
-                          return (
-                            <MenuItem key={element.id} value={element.id}>
-                              {element.name}
-                            </MenuItem>
-                          )
-                        })
-                      }
-                      </Select>
-                    </FormControl>
-                  </MDBox>
-                  {showName ? (
-                    <MDBox pt={4} pb={3} px={3}>
-                    <MDBox sx={{ textAlign: "center", mb: 2 }}>
-                      <MDTypography mb={2}
-                        variant="subtitle1"
-                      >Agora, digite o seu nome completo e sua data de nascimento
+                  <Card>
+                    <MDBox
+                      variant="gradient"
+                      bgColor="dark"
+                      borderRadius="lg"
+                      coloredShadow="info"
+                      mx={2}
+                      mt={-3}
+                      p={2}
+                      mb={1}
+                      textAlign="center"
+                    >
+                      <MDTypography variant="h5" fontWeight="medium" color="white" mt={1}>
+                        Diga-nos qual o grupo você está!
                       </MDTypography>
                     </MDBox>
-                    <MDBox mb={2}>
-                      <MDInput type="text" label="Nome Completo" fullWidth />
-                      <MDInput type="text" label="Nome Completo" fullWidth />
+                    <MDBox px={3} >
+                      <FormControl variant="standard" fullWidth sx={{ m: 1, minWidth: 120 }}>
+                        <InputLabel sx={{ p: 1 }}>Selecione um grupo</InputLabel>
+                        <Select
+                          labelId="demo-simple-select-standard-label"
+                          id="demo-simple-select-standard"
+                          value={idGroup}
+                          onChange={handleChange}
+                          label="Selecione um grupo"
+                          fullWidth
+                          name="group_id"
+                          sx={{
+                            minHeight: '40px',
+                            mt: 1
+                          }}
+                        >
+                          <MenuItem value="0">
+                            <em>--Selecione--</em>
+                          </MenuItem>
+                        {
+                          nameGroup?.map(element => {
+                            return (
+                              <MenuItem key={element.id} value={element.id}>
+                                {element.name}
+                              </MenuItem>
+                            )
+                          })
+                        }
+                        </Select>
+                      </FormControl>
                     </MDBox>
-                    <MDBox px={3} mb={2}>
-                      <MDButton variant="gradient" color="info" fullWidth>Próximo passo</MDButton>
-                    </MDBox>
-                    <MDBox sx={{ textAlign: "center", textAlign: 'right', mr: 1 }}>
-                      <MDTypography
-                        variant="overline"
-                      >Passo 1 de 4
-                      </MDTypography>
-                    </MDBox>
-                  </MDBox>
-                  ): null}
-                </Card>
+                    {showName ? (
+                      <MDBox pt={4} pb={3} px={3}>
+                      <MDBox sx={{ textAlign: "center", mb: 2 }}>
+                        <MDTypography mb={2}
+                          variant="subtitle1"
+                        >Agora, digite o seu nome completo e sua data de nascimento
+                        </MDTypography>
+                      </MDBox>
+                      <MDBox mb={2}>
+                        <TextField
+                          label="Nome Completo"
+                          fullWidth
+                          onChange={(e) => setNameUser(e.target.value)}                
+                          value={nameUser}
+                          />
+                        <InputMask
+                          mask="99/99/9999"
+                          onChange={onChangeDate}
+                          disabled={false}
+                          maskChar=" "
+                        >
+                          {() => 
+                            <TextField
+                              ref="brithDate"
+                              name="brithDate"
+                              type="text"
+                              label="Nascido em"
+                              fullWidth
+                              sx={{
+                                mt: 1
+                              }}
+                            
+                            />}
+                        </InputMask>
+                      </MDBox>
+                      <MDBox px={3} mb={2}>
+                        <MDButton
+                          variant="gradient"
+                          color="info"
+                          fullWidth
+                          onClick={salvarDados}
+                        >
+                            Próximo passo
+                        </MDButton>
 
+                      </MDBox>
+                      <MDBox sx={{ textAlign: "center", textAlign: 'right', mr: 1 }}>
+                        <MDTypography
+                          variant="overline"
+                        >Passo 1 de 4
+                        </MDTypography>
+                      </MDBox>
+                    </MDBox>
+                    ): null}
+                  </Card>
+                
               </MDBox>
 
             </Container>
