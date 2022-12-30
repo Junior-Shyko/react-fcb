052993cbdcb52b91101c8aadd4d4204f7a3becb6
JuinEsp <franciscoanto@gmail.com>
2022-12-30 11:03:35 -0300

    Alterando endereço da api

--- src/services/Api.js
+++ src/services/Api.js
@@ -2,5 +2,5 @@ import axios from "axios";
 
 // export const urlBase = 'http://localhost/api/v1';
 export const api = axios.create({
-    baseURL: "http://192.168.30.124/api/v1/",
+    baseURL: "http://159.203.69.144:38000/api/v1/",
 });
